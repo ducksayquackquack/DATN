ff6c571637b52eed4db7503dea178c014e017caa
unknown <sonvu3338@gmail.com>
2026-01-29 16:40:56 +0700

    DATN

---
+++ src/main/java/org/example/datnnhom03/Repsotiory/MauSacRepository.java
@@ -0,0 +1,9 @@
+package org.example.datnnhom03.Repsotiory;
+
+import org.example.datnnhom03.Model.MauSac;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MauSacRepository extends JpaRepository<MauSac, Integer> {
+}
